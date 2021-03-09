b09e0081832e17255e363a6ff992f893e4281103
Matteo <matteomigliori280489@gmail.com>
2021-03-09 14:45:47 +0100

    inizio

---
+++ js/main.js
@@ -0,0 +1,4 @@
+// un alert mostra 5 numeri generati casualmente
+//parte un timer di 30 secondi visibile
+// finiti i 30 secondi chiedere all'utente di inserire i 5 numeri che sono apparsi nell'alert
+// dire quanti numeri ha indovinato l'utente
